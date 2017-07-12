c1b284754e3c09fe92773f40a7500f4c9ca38873
Steven Hodgkinson <guitarist_7@yahoo.com>
2017-07-12 23:54:42 +0100

    Firstcommit

--- FirstXcodeProject/ViewController.swift
+++ FirstXcodeProject/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHelloWorld: UILabel!
+    
+    var tapcount = 0
+    
+    @IBAction func pushmeTapped(_ sender: Any) {
+        tapcount = tapcount + 1
+        print(tapcount)
+        if tapcount >= 10 {
+            lblHelloWorld.text = "You tapped the button 10 times!"
+        }
+        
+        //lblHelloWorld.text = "Hello There!"
+        print("Button tapped")
+    }
+
+    @IBAction func coolTapped(_ sender: Any) {
+        lblHelloWorld.text = "Buttons are cool!"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
